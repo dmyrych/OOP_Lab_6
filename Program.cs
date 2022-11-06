a223707176119d3abdbae8836021d3ad02f41826
dmyrych <dmyrych17@gmail.com>
2022-11-06 14:22:06 +0200

    first task done!

--- Program.cs
+++ Program.cs
@@ -1,11 +1,21 @@
-namespace Base
+namespace Based
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-        
+        MyList<int> list = new MyList<int>();
+        list.Add(12);
+        list.Add(42);
+        list.Replace(0, 1);
+        Console.WriteLine($"Length of list is: {list.Capacity()}");
+        Console.WriteLine($"Element of index 0 is: {list.Indexator(0)}");
+        Console.WriteLine($"Length of list is: {list.Capacity()}");
+        Console.WriteLine($"Element of index 1 is: {list.Indexator(1)}");
+        Console.WriteLine($"Length of list is: {list.Capacity()}");
+        Console.WriteLine($"Element of index 2 is: {list.Indexator(2)}");
+        Console.WriteLine("End of Task 1!");
+
         
         }
     }
